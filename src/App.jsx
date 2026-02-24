1647e52f9a63cf004be9e45a735105203c735f38
Abdelrhman Ehab <123629461+abdelrhman-ehab@users.noreply.github.com>
2026-02-24 03:10:14 +0200

    Add functionality to fetch random Ayah from API
    Fetch and display a random Ayah from the Quran every 30 seconds.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import Tasks from './components/Tasks'
 import TasksInput from './components/TasksInput';
 import { Toaster } from 'react-hot-toast';
+const getAya = async () => {
+const res = await fetch(`https://api.alquran.cloud/v1/ayah/${(Math.random() * 6236) + 1}`, {
+        cache: 'no-cache'
+    })
+    const response = await res.json()
+    toast.dismiss()
+    toast(response?.data?.text, { position: 'top-center', duration: 10000 })
+    console.log(response.data.number)
+}
+
+getAya()
+
+setInterval(() => {
+    getAya()
+}, 30000)
 
 function App() {
   return <>
