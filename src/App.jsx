675d38db0726b7f7b5cbaf780442134c3f52b375
abdelrhman-ehab <abdelrhmanehab2904@gmail.com>
2026-02-25 14:54:04 +0200

    sub tasks

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,26 @@ import TasksInput from './components/TasksInput';
 import { Toaster } from 'react-hot-toast';
 import toast from 'react-hot-toast';
 const getAya = async () => {
-const res = await fetch(`https://api.alquran.cloud/v1/ayah/${(Math.random() * 6236) + 1}`, {
-        cache: 'no-cache'
-    })
-    const response = await res.json()
-    toast.dismiss()
-    toast(response?.data?.text, { position: 'top-center', duration: 10000 })
-    console.log(response.data.number)
+  const res = await fetch(`https://api.alquran.cloud/v1/ayah/${(Math.random() * 6236) + 1}`, {
+    cache: 'no-cache'
+  })
+  const response = await res.json()
+  toast.dismiss()
+  toast(response?.data?.text, { position: 'top-center', duration: 15000 })
 }
 
 getAya()
 
 setInterval(() => {
-    getAya()
-}, 30000)
+  getAya()
+}, 60000)
 
 function App() {
   return <>
-    <div className='min-h-fit min-w-78.5 w-full max-w-4xl p-3 space-y-3 grid grid-cols-1 fixed left-1/2 -translate-x-1/2 top-0 bottom-0 '>
+    <div className='min-h-screen min-w-78.5 w-full max-w-4xl px-3 pt-3 space-y-3 grid grid-cols-1 fixed left-1/2 -translate-x-1/2 top-0 bottom-0 '>
       <TasksInput />
       <Tasks />
-      <Toaster/>
+      <Toaster />
     </div>
   </>
 }
--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -6,48 +6,139 @@ export const useToDoStore = create((set) => ({
     todos: JSON.parse(localStorage.getItem('todos')) || [],
 
     // add todo
-    addTodo: (todoTitle, todoBody) => set(obj => {
-        const newTodos = [...obj.todos, { id: Date.now(), title: todoTitle, body: todoBody, completed: false }]
+    addTodo: (todoTitle, tododescreption) => set(state => {
+        const newTodos = [...state.todos, {
+            id: Date.now(),
+            title: todoTitle,
+            body: tododescreption,
+            completed: false,
+            subTodos: []
+        }]
         localStorage.setItem('todos', JSON.stringify(newTodos))
         return { todos: newTodos }
     }),
 
     // remove todo
-    removeTodo: (todoId) => set(obj => {
-        const newTodos = obj.todos.filter(todo => todo.id !== todoId)
+    removeTodo: (todoId) => set(state => {
+        const newTodos = state.todos.filter(todo => todo.id !== todoId)
         localStorage.setItem('todos', JSON.stringify(newTodos))
         return { todos: newTodos }
     }),
 
     // set todo completed
-    completedTodo: (todoId) => set(obj => {
-        const newTodos = obj.todos.map(todo => todoId === todo.id ? { ...todo, completed: !todo.completed } : todo)
+    completedTodo: (todoId) => set(state => {
+        const newTodos = state.todos.map(todo => todoId === todo.id ? { ...todo, completed: !todo.completed } : todo)
         localStorage.setItem('todos', JSON.stringify(newTodos))
         return { todos: newTodos }
     }),
 
     // update todo
-    updateTodo: (todoId, todoTittle, todoBody) => set(obj => {
-        const newTodos = obj.todos.map(todo =>
+    updateTodo: (todoId, todoTittle, todoBody) => set(state => {
+        const newTodos = state.todos.map(todo =>
             todo.id === todoId ? { ...todo, title: todoTittle, body: todoBody } : todo)
         localStorage.setItem('todos', JSON.stringify(newTodos))
         return { todos: newTodos }
     }),
 
+    // add sub todos
+    addSubTodos: (todoId, subTodoData) => set((state) => {
+        const newTodos = state.todos.map((todo) => {
+            if (todoId === todo.id) {
+                return {
+                    ...todo,
+                    subTodos: [
+                        ...todo.subTodos,
+                        {
+                            id: Date.now(),
+                            ...subTodoData,
+                            completed: false
+                        }
+                    ]
+                }
+            }
+            return todo
+        })
+        localStorage.setItem('todos', JSON.stringify(newTodos))
+        return { todos: newTodos }
+    }),
+
+    // edit sub todos
+    UpdateSubTodo: (todoId, subTodoId, updatedData) => set((state) => {
+        const newTodos = state.todos.map((todo) => {
+            if (todo.id === todoId) {
+                return {
+                    ...todo,
+                    subTodos: todo.subTodos.map((sub) => {
+                        if (subTodoId === sub.id) {
+                            return {
+                                ...sub,
+                                ...updatedData
+                            }
+                        }
+                        return sub
+                    })
+                }
+            }
+            return todo
+        })
+        localStorage.setItem('todos', JSON.stringify(newTodos))
+        return { todos: newTodos }
+    }),
+
+    // chicked sub todo
+    completedSubTodo: (todoId, subTodoId) => set((state) => {
+        const newTodos = state.todos.map((todo) => {
+            if (todoId === todo.id) {
+                return {
+                    ...todo,
+                    subTodos: [
+                        ...todo.subTodos.map((sub) => {
+                            if (subTodoId === sub.id) {
+                                return {
+                                    ...sub,
+                                    completed: !sub.completed
+                                }
+                            }
+                            return sub
+                        })
+                    ]
+                }
+            }
+            return todo
+        })
+        localStorage.setItem('todos', JSON.stringify(newTodos))
+        return { todos: newTodos }
+    }),
+
+    // delete sub todos
+    deleteSubTodo: (todoId, subTodoId) => set((state) => {
+        const newTodos = state.todos.map((todo) => {
+            if (todoId === todo.id) {
+                return {
+                    ...todo,
+                    subTodos: todo.subTodos.filter((sub) => sub.id !== subTodoId)
+                }
+            }
+            return todo
+        })
+        localStorage.setItem('todos', JSON.stringify(newTodos))
+        return { todos: newTodos }
+    }),
+
     // get all todos
-    getAllTodos: () => set(obj => {
+    getAllTodos: () => set(state => {
         return { todos: JSON.parse(localStorage.getItem('todos')) }
     }),
 
     // get completed todos
-    getCompletedTodos: () => set(obj => {
+    getCompletedTodos: () => set(state => {
         const todos = JSON.parse(localStorage.getItem('todos')) || []
         const completedTodos = todos?.filter(todo => todo.completed === true)
         return { todos: completedTodos }
     }),
 
     // get uncompleted todos
-    getUncompletedTodos: () => set(obj => {
+    getUncompletedTodos: () => set(state => {
         const todos = JSON.parse(localStorage.getItem('todos')) || []
         const uncompletedTodos = todos?.filter(todo => todo.completed === false)
         return { todos: uncompletedTodos }
@@ -62,3 +153,4 @@ export const useToDoStore = create((set) => ({
     }))
 
 }))
+
